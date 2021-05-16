ca4543ff0c987750585d722865dddccf46080150
Pum <28806828+PumPum7@users.noreply.github.com>
2021-05-16 16:31:37 +0200

    updated getUrl function

--- api/getUrl.go
+++ api/getUrl.go
@@ -10,43 +10,44 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-
-type URL struct {
-	Short string `fauna:"short" json:"short"`
-	Long string `fauna:"long" json:"long"`
-	Usage int `fauna:"usage" json:"usage"`
-	Password string `fauna:"password" json:"-"`
-	Protected bool `json:"protected"`
+type getUrl struct {
+	Short      string `fauna:"short" json:"short"`
+	Long       string `fauna:"long" json:"long"`
+	Usage      int    `fauna:"usage" json:"usage"`
+	Password   string `fauna:"password" json:"-"`
+	Protected  bool   `json:"protected"`
+	Expiration int    `json:"expiration,omitempty"`
 }
+
+// TODO: add support for returning the expiration time
 
 type ShortURL struct {
 	Short string `json:"url"`
 }
 
-
 func GetURL(w http.ResponseWriter, r *http.Request) {
 	shortUrl := r.FormValue("url")
 	if shortUrl == "" {
-		returnError(fmt.Errorf("no url specified"), w)
+		returnGetError(fmt.Errorf("no url specified"), w)
 	}
 
 	key := os.Getenv("GO_FAUNA_SECRET_KEY_A")
-	if (key == "") {
-		returnError(fmt.Errorf("missing GO_FAUNA_SECRET_KEY_A"), w)
+	if key == "" {
+		returnGetError(fmt.Errorf("missing GO_FAUNA_SECRET_KEY_A"), w)
 
 	}
 	client := f.NewFaunaClient(key)
 
 	res, err := client.Query(f.Get(f.MatchTerm(f.Index("url_short"), shortUrl)))
 	if err != nil {
-		returnError(err, w)
+		returnGetError(err, w)
 	}
 
-	var url URL
+	var url getUrl
 
 	err = res.At(f.ObjKey("data")).Get(&url)
 	if err != nil {
-		returnError(err, w)
+		returnGetError(err, w)
 	}
 	if url.Password != "" {
 		url.Protected = true
@@ -58,7 +59,7 @@ func GetURL(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, string(msg))
 }
 
-func returnError(err error, w http.ResponseWriter) {
+func returnGetError(err error, w http.ResponseWriter) {
 	fmt.Fprintf(w, err.Error())
 	log.Fatal(err)
 }
