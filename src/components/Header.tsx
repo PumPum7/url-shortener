b09b7a0f1f286781997b2b26e5fee931fe479641
Pum <28806828+PumPum7@users.noreply.github.com>
2021-06-01 10:45:45 +0200

    security and accessibility improvements

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,11 +10,12 @@ export const Header = (): JSX.Element => {
             <div className="flex flex-wrap justify-between items-center">
                 <div>
                     <a href="/">
-                        <span className="text-gray-900 inline-flex items-center font-semibold text-3xl">
+                        <span className="text-gray-900 inline-flex items-center font-semibold text-xl md:text-3xl">
                             <Image
                                 src={"/scissors.png"}
                                 width={"44"}
                                 height={"44"}
+                                alt="Logo image"
                             />
                             URL Cutter
                         </span>
@@ -25,10 +26,17 @@ export const Header = (): JSX.Element => {
                         <li className="mr-5 hidden md:block">
                             <a
                                 href="https://github.com/PumPum7/url-shortener"
-                                target="_blank">
+                                target="_blank"
+                                rel="noreferrer"
+                                >
                                 GitHub
                             </a>
                         </li>
+                        {user && (
+                            <li className="mr-5 hidden md:block">
+                                <a href={"/api/auth/logout"}>Logout</a>
+                            </li>
+                        )}
                         <li className="rounded-md bg-gradient-to-r from-blue-400 to-indigo-400 py-2 px-3">
                             {user ? (
                                 <a href={"/profile"}>{user.nickname}</a>
