0fd45949f790018f840d7794db54e10c2ae74c25
Pum <28806828+PumPum7@users.noreply.github.com>
2025-03-03 17:15:46 +0100

    fix lint error

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -165,7 +165,7 @@ export default function LoginPage() {
 
                 <div className="text-center mt-4">
                     <p className="text-sm text-gray-600">
-                        Don't have an account?{" "}
+                        Don&apos;t have an account?{" "}
                         <Link
                             href="/register"
                             className="font-medium text-indigo-600 hover:text-indigo-500">
